d9d81671d588e86a7eec2ee9e2bf1145c94a45ef
Arantxa Ordoyo <44567127+arianxa@users.noreply.github.com>
2025-11-28 18:56:10 +0000

    final

---
+++ .learn/resets/04-our-first-function/app.js
@@ -0,0 +1,6 @@
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
